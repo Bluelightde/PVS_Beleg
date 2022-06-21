e1de14840fb6c0964f3fac696d616c37d90c9d8f
Miguel Gomez <61145742+miguelgr2501@users.noreply.github.com>
2022-06-21 10:47:02 +0200

    client update

--- Beleg/Beleg/Client.java
+++ Beleg/Beleg/Client.java
@@ -5,16 +5,12 @@ import java.io.OutputStream;
 import java.net.Socket;
 
 public class Client {
-	 static Presenter p = new Presenter();
-	 static View v = new View(p);
-	 static Model m = new Model(v);
-	
 	public static void main(String args[]) throws Exception {
-		
-		
-		Socket client = new Socket("127.0.0.1",6000);
-	
+		Presenter p = new Presenter();
+		View v = new View(p);
+		Model m = new Model(v);
 		p.setModelAndView(m, v);
+
 		double cr = p.getCr();
 		double ci = p.getCi();
 
@@ -24,25 +20,25 @@ public class Client {
 		long lng1 = Double.doubleToLongBits(cr);
 		long lng2 = Double.doubleToLongBits(ci);
 
-		for(int i = 0; i < 8; i++) {
-			coord1[i] = (byte)((lng1 >> ((7 - i) * 8)) & 0xff);
-			coord2[i] = (byte)((lng2 >> ((7 - i) * 8)) & 0xff);
+		for (int i = 0; i < 8; i++) {
+			coord1[i] = (byte) ((lng1 >> ((7 - i) * 8)) & 0xff);
+			coord2[i] = (byte) ((lng2 >> ((7 - i) * 8)) & 0xff);
 		}
 
-		OutputStream out = client.getOutputStream(); 
-		DataOutputStream dos = new DataOutputStream(out);
-	
 		byte[] coord = new byte[16];
-		System.arraycopy(coord1, 0, coord, 0, coord1.length);  
+		System.arraycopy(coord1, 0, coord, 0, coord1.length);
 		System.arraycopy(coord2, 0, coord, 0, coord2.length);
+
+		System.out.println(coord);
+
+		Socket client = new Socket("127.0.0.1", 6000);
+
+		OutputStream out = client.getOutputStream();
+		DataOutputStream dos = new DataOutputStream(out);
 
 		dos.write(coord, 0, coord.length);
 		client.close();
-			
+
 	}
 
 }
-	
-	
-
-
